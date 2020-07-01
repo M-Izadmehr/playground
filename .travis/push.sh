00e438c19353215620a81ffb56ab1d9e34dd912b
Mojtaba Izadmehr <mojtaba.izadmehr@new10.com>
2020-07-02 01:35:47 +0200

    playing with travis

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "$EMAIL"
+  git config --global user.name "Mojtaba Izadmehr"
+}
+
+commit_website_files() {
+  git checkout master
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/m-izadmehr/playground.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+setup_git
+commit_website_files
+upload_files
